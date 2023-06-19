cd4f9118562613745309dc01aba8131aeec9a274
JelliChu <znchapman@gmail.com>
2023-06-19 23:38:18 +0100

    Update AsyncRelayCommand.cs

--- MvvmFramework/MvvmFramework/RelayCommands/AsyncRelayCommand.cs
+++ MvvmFramework/MvvmFramework/RelayCommands/AsyncRelayCommand.cs
@@ -10,7 +10,7 @@ public class AsyncRelayCommand : BaseCommand
 		_execute = execute;
 	}
 
-	public override void Execute() => AsyncHelpers.RunSync(ExecuteAsync);
+	public override async void Execute() => await ExecuteAsync();
 
 	public async Task ExecuteAsync()
 	{
@@ -39,7 +39,7 @@ public class AsyncRelayCommand<TParameter> : BaseCommand<TParameter>
 
     public AsyncRelayCommand(Func<TParameter, Task> execute) => _execute = execute;
 
-    public override void Execute(TParameter parameter) => AsyncHelpers.RunSync(() => ExecuteAsync(parameter));
+	public override async void Execute(TParameter parameter) => await ExecuteAsync(parameter);
 
     public async Task ExecuteAsync(TParameter parameter)
     {
